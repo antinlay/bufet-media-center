d4147f5c98d2f16b3ff2439bbeaf1ba54ccbb154
Janiecee <janiecee@student.21-school.ru>
2026-07-16 23:16:54 +0300

    Обновил контейнер API и демо-данные
    - apps/api/Dockerfile: добавил PostgreSQL-библиотеки и указал порт Puma 3000 для reverse proxy
    - apps/api/db/seeds.rb: удалил генерацию устаревших demo feeds, screens и content
    - apps/api/db/seeds.rb: добавил очистку ранее созданных demo-групп и системного контента

--- apps/api/db/seeds.rb
+++ apps/api/db/seeds.rb
@@ -15,6 +15,18 @@ Group.find_or_create_by!(name: Group::SYSTEM_ADMIN_GROUP_NAME, description: "Sys
 
 system_user = User.find_or_create_by!(is_system_user: true, first_name: "Concerto", last_name: "System User")
 
+legacy_demo_group_names = [ "Demo Feed Owners", "Demo Screen Owners" ]
+legacy_demo_content_names = [
+  "Welcome Ticker",
+  "HTML Ticker",
+  "GenAI Poster",
+  "Welcome Robot",
+  *(1..6).map { |index| "Landscape #{index}" }
+]
+
+Group.where(name: legacy_demo_group_names).find_each(&:destroy!)
+Content.where(user: system_user, name: legacy_demo_content_names).find_each(&:destroy!)
+
 puts "Seeding application settings..."
 
 initial_settings = {
@@ -66,81 +78,4 @@ end
 
 if template.persisted? && Setting[:default_template_id].blank?
   Setting[:default_template_id] = template.id
-end
-
-
-puts "Seeding demo feeds..."
-
-demo_feed_group = Group.find_or_create_by!(name: "Demo Feed Owners", description: "Managers of the Demo Feeds.")
-general_feed = Feed.find_or_create_by!(name: "General", group: demo_feed_group)
-
-rss_feed = RssFeed.find_or_initialize_by(name: "Yahoo News", description: "The latest news and headlines from Yahoo! News' RSS feed.", group: demo_feed_group)
-if rss_feed.new_record?
-    rss_feed.url = "http://news.yahoo.com/rss"
-    rss_feed.save!
-
-    suppress(Exception) do
-      rss_feed.refresh
-    end
-end
-
-landscape_feed = Feed.find_or_create_by!(name: "Landscapes", group: demo_feed_group)
-
-puts "Seeding demo screen..."
-
-demo_screen_group = Group.find_or_create_by!(name: "Demo Screen Owners", description: "Managers of the Demo Screen.")
-screen = Screen.find_or_create_by!(name: "Demo Screen", template: template, group: demo_screen_group)
-if screen.subscriptions.empty?
-    screen.subscriptions.new(feed: general_feed, field: main_field)
-    screen.subscriptions.new(feed: general_feed, field: ticker_field)
-    screen.subscriptions.new(feed: landscape_feed, field: main_field)
-    screen.subscriptions.new(feed: rss_feed, field: sidebar_field)
-    screen.save!
-end
-
-FieldConfig.find_or_create_by!(screen: screen, field: time_field, pinned_content: clock)
-
-puts "Seeding demo content..."
-
-plain_ticker = RichText.find_or_initialize_by(name: "Welcome Ticker", text: "Welcome to Concerto!", user: system_user)
-if plain_ticker.new_record?
-    plain_ticker.render_as = RichText.render_as[:plaintext]
-    plain_ticker.submissions.new(feed: general_feed)
-    plain_ticker.save!
-end
-
-html_ticker = RichText.find_or_initialize_by(name: "HTML Ticker", text: "<b>Concerto</b> is digital signage for <i>everyone</i>.", user: system_user)
-if html_ticker.new_record?
-    html_ticker.render_as = RichText.render_as[:html]
-    html_ticker.submissions.new(feed: general_feed)
-    html_ticker.save!
-end
-
-graphic1 = Graphic.find_or_initialize_by(name: "GenAI Poster", duration: 30, user: system_user)
-if graphic1.new_record?
-    graphic1.image = File.new("db/seed_assets/genai_poster.png")
-    graphic1.submissions.new(feed: general_feed)
-    graphic1.save!
-end
-
-graphic2 = Graphic.find_or_initialize_by(name: "Welcome Robot", duration: 25, user: system_user)
-if graphic2.new_record?
-    graphic2.image = File.new("db/seed_assets/welcome_robot.jpg")
-    graphic2.submissions.new(feed: general_feed)
-    graphic2.save!
-end
-
-(1..6).each do |i|
-  landscape = Graphic.find_or_initialize_by(name: "Landscape #{i}", duration: 10 + 2*i, user: system_user)
-  if landscape.new_record?
-    landscape.image = File.new("db/seed_assets/landscape_#{i}.jpg")
-    landscape.submissions.new(feed: landscape_feed)
-    landscape.save!
-  end
-end
-
-Graphic.transaction do
-  Graphic.all.each do |g|
-    g.image.analyze if !g.image.analyzed?
-  end
 end
